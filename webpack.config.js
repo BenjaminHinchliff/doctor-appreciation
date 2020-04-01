cc1d986cb64984aa9b78b27ba5c6da5a03a1b404
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-04-01 12:42:57 -0700

    added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
 };
